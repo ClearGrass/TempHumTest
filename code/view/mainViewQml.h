9c5a5a01d1161a4bb6d141d17eb933955986f97e
Jason <15264316179@163.com>
2017-09-30 15:14:44 +0800

    四中模式标准程序

--- code/view/mainViewQml.h
+++ code/view/mainViewQml.h
@@ -34,7 +34,7 @@
 #define POWERON_CONFIRM_INTERVER     (60 * 1000 * 30)
 #define TAP_INTERVAL                 (5 * 1000)
 #define FRE_INTERVAL                 (10 * 1000)
-#define VERSION_NO                   "charging_discharging_test_2"
+#define VERSION_NO                   "four-modes-test"
 
 #include "driverWiFi.h"
 
